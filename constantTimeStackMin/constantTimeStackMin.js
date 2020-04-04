4c7029f9729c93aa971ff222416eb6f24971eb4f
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-04-03 20:31:56 -0700

    fixed the failing test case

--- constantTimeStackMin/constantTimeStackMin.js
+++ constantTimeStackMin/constantTimeStackMin.js
@@ -21,38 +21,44 @@
 
   this.storage = {};
   this.storage1 = {};
-  this.length = 0;
+  this.storagelength = 0;
+  this.storage1length = 0;
 
 // add an item to the top of the stack
   this.push = function(value) {
-    if(this.length === 0) {
-      this.storage[this.length] = value;
-      this.storage1[this.length] = value;
-    } else if(value < this.storage1[this.length-1]) {
-      this.storage1[this.length] = value;
+    this.storage[this.storagelength] = value;
+    this.storagelength++;
+    if(this.storage1length === 0) {
+      this.storage1[this.storage1length] = value;
+      this.storage1length++;
+    } else if(value < this.storage1[this.storage1length-1]) {
+      this.storage1[this.storage1length] = value;
+      this.storage1length++;
     }
-    this.length++;
+
   };
 
 // remove an item from the top of the stack
   this.pop = function() {
-    if(this.length > 0) {
-      if(this.storage[this.length] === this.storage1[this.length]) {
-        delete this.storage1[this.length - 1];
-        delete this.storage1[this.length - 1];
-        this.length--;
+    if(this.storagelength > 0) {
+      var poppedElement = this.storage[this.storagelength - 1];
+      delete this.storage[this.storagelength-1];
+      this.storagelength--;
+      if(poppedElement === this.storage1[this.storage1length]) {
+        delete this.storage1[this.storage1length - 1];
+        this.storage1length--
       }
     }
   };
 
 // return the number of items in the stack
   this.size = function() {
-    return this.length;
+    return this.storagelength;
   };
 
 // return the minimum value in the stack
   this.min = function() {
-    return this.storage1[this.length-1];
+    return this.storage1[this.storage1length-1];
   };
 
 };
@@ -63,7 +69,7 @@ example.push(3)
 example.push(2)
 example.push(5)
 example.pop(2)
-//example.size()
+example.size()
 console.log(example.min()) // 2
 example.push(3)
 example.push(2)

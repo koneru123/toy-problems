f8143b7aa6704ce71838f91278050af2a7d081c6
koneru123 <koneru.priyanka@gmail.com>
2020-04-21 19:55:42 -0700

    Binary Search Array

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -10,6 +10,24 @@
  * console.log(index); // null
  */
 
-var binarySearch = function (array, target) {
+var binarySearch = function (array, target) { 
+    let start = 0; 
+    let end = array.length - 1; 
+    while(start < end) {
+        let middleValue = Math.floor((start+end)/2); 
+        if(target === array[middleValue]) {
+            return middleValue;
+        }
+        else if(target < array[middleValue]) { 
+            end = middleValue;
+        }
+        else if(target > array[middleValue]){ 
+            start = middleValue; 
+        } else {
+            return null;
+        }
+    }
 };
 
+//console.log(binarySearch([1, 2, 3, 4, 5], 4));
+console.log(binarySearch([1, 2, 3, 4, 5], 8));
